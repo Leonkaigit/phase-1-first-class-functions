9692283dacd77fbce5d92c15736b0fb09b3ad1a4
LeonGitonga <leon.gitonga@student.moringaschool.com>
2023-09-20 14:07:08 +0300

    Complete assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+function callback() {
+    console.log("Please come back function.");
+}
+
+function receivesAFunction(callback) {
+     return callback();
+}
+returnsANamedFunction(); 
+function returnsANamedFunction() {
+return function sendBack()  {
+
+}
+}
+function returnsAnAnonymousFunction () {
+    return()=> {
+        
+    }
+}
+
+
+
+
+
